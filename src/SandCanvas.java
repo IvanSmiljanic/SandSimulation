b491365e0d8bc684a637deaafb9e8370b45bf293
Ivan Smiljanic <ivan.smiljanic78@gmail.com>
2023-01-24 05:55:52 +0000

    Added JFrame and Canvas

---
+++ src/SandCanvas.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class SandCanvas extends Canvas
+{
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.black);
+        g.drawLine(20, 20, 400, 400);
+    }
+}
