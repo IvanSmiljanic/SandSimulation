779bc654d61dc5a905436fe7e6811575aca69953
Ivan Smiljanic <ivan.smiljanic78@gmail.com>
2023-01-24 05:56:58 +0000

    Forgot to add Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        JFrame win = new JFrame("Sand Simulation");
+        SandCanvas canvas = new SandCanvas();
+
+        win.setVisible(true);
+        win.setSize(1920, 1080);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        win.add(canvas);
     }
 }
